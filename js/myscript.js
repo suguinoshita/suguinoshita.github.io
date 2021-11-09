3d95c1e55b8777b3ccc5b576706a4c9a38bc270a
suguinoshita <93954848+suguinoshita@users.noreply.github.com>
2021-11-09 14:31:29 -0300

    update2

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,8 @@
 function myFunction(){
     var input1;
     var output1;
-    input1 = document.getElementsByName("Sec1dim1";
-    output1 = document.getElementsByName("Output1";
-    var out1 = input1.value;
-    output1.value = "result is " + out1;
+    in1 = document.getElementsByName("Sec1dim1").value;
+    var out1 = in1;
+    document.getElementsByName("Output1").value = "result is " + out1;
 
 }
