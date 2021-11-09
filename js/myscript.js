80afcec16a246392c76c8c668763b7e9a32d8ead
suguinoshita <93954848+suguinoshita@users.noreply.github.com>
2021-11-09 14:19:49 -0300

    update

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var input1;
+    var output1;
+    input1 = document.getElementsByName("Sec1dim1";
+    output1 = document.getElementsByName("Output1";
+    var out1 = input1.value;
+    output1.value = "result is " + out1;
+
+}
